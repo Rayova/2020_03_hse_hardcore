bebd5a48a7d1e458282ad731499aecc1cac175e4
Rayova <55283119+Rayova@users.noreply.github.com>
2020-03-12 17:02:16 +0300

    nj

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
 // // circle13
-// let circleAnimation = anime({
-//   targets: circle,
-//   rotate: [0, 360],
-//   easing: 'linear',
-//   duration: 50000,
-//   loop: true
-// })
-//
+let circleAnimation = anime({
+  targets: circle,
+  rotate: [0, 360],
+  easing: 'linear',
+  duration: 50000,
+  loop: true
+})
+
 
 // // icon_12
-// let iconAnimation = anime ({
-//   targets: icon,
-//   rotate: [0, 360],
-//   autoplay: false,
-//   duration: 2000
-// })
-// let icon01 = document.getElementById('icon')
-// icon01.onclick = iconAnimation.play
+let iconAnimation = anime ({
+  targets: icon,
+  rotate: [0, 360],
+  autoplay: false,
+  duration: 2000
+})
+let icon01 = document.getElementById('icon')
+icon01.onclick = iconAnimation.play
 
 // alert
 function myFunction() {
-  let person = prompt("Please enter your name", "Hot stuff");
-  if (person != null) {
-    document.getElementById("guest").innerHTML =
-    "Hello " + person + "! How are you today?";
+  let guest = prompt("Please enter your name", "Hot stuff");
+  if (guest != null) {
+    document.getElementById("guest").innerHTML = guest
   }
+  return guest
 }
