c60f77779a89768edb32f6b32dde8407f182715a
Rayova <55283119+Rayova@users.noreply.github.com>
2020-03-20 15:20:11 +0300

    hot stuff

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 // circle13
-let circleAnimation = anime({
-  targets: circle,
-  rotate: [0, 360],
-  easing: 'linear',
-  duration: 50000,
-  loop: true
-});
+// let circleAnimation = anime({
+//   targets: circle,
+//   rotate: [0, 360],
+//   easing: 'linear',
+//   duration: 50000,
+//   loop: true
+// });
 //
 // icon_12
 // let iconAnimation = anime ({
@@ -17,26 +17,27 @@ let circleAnimation = anime({
 // let icon01 = document.getElementById('icon')
 // icon01.onclick = iconAnimation.play
 
-// // alert не работает
+// alert
+
+  let guest1 = prompt("Please enter your name", "Hot stuff");
+  if (guest1 != null) {
+    document.getElementById("guest").innerHTML = guest1
+  }
+
+
+// // кнопка блоки
+// let blocks = document.querySelector('.blocks');
+// let icon = document.querySelector('.icon');
+// blocks.style.display = 'none'
 //
-//   let guest1 = prompt("Please enter your name", "Hot stuff");
-//   if (guest1 != null) {
-//     document.getElementById("guest").innerHTML = guest1
-//   }
+// icon.addEventListener("click",      function() {
 //
+//     if(blocks.style.display == 'none')  blocks.style.display = 'block';
+//     else blocks.style.display = 'none'
 //
-// кнопАчка больше не вертится
-let blocks = document.querySelector('.blocks');
-let icon = document.querySelector('.icon');
-blocks.style.display = 'none'
-
-icon.addEventListener("click",      function() {
-
-    if(blocks.style.display == 'none')  blocks.style.display = 'block';
-    else blocks.style.display = 'none'
-
-  });
-
+//   });
+//
+// // кнопка  прямоугольники
 // let rectangles = document.querySelector('.rectangles');
 // let tool2 = document.querySelector('.tool_2');
 // rectangles.style.display = 'none'
